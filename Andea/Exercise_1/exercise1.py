6b6da2abf2c8d186e4df90af7881c6d7ed660609
/DominikMalinowski <malinowskidominik00@gmail.com>
2022-05-16 20:38:54 +0200

    update

--- Andea/Exercise_1/exercise1.py
+++ Andea/Exercise_1/exercise1.py
@@ -8,7 +8,6 @@ If the number is a multiple of 4, print out a different message.
 Ask the user for two numbers: one number to check (call it num) and one number to divide by (check).
 If check divides evenly into num, tell that to the user. If not, print a different appropriate message.
 
-show how git works
 """
 
 
