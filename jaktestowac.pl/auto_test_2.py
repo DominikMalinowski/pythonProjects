391ca78bc21af5cc0712a782bc225ea7a0ff8eed
/DominikMalinowski <malinowskidominik00@gmail.com>
2022-07-03 10:31:30 +0200

    update 2

--- jaktestowac.pl/auto_test_2.py
+++
@@ -1,9 +0,0 @@
-
-# import of selenium and webdriver
-import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import WebDriverException
-
-# path to webdriver
-driver = webdriver.Chrome(service=Service(r'D:\ChromeDriver\chromedriver.exe'))
