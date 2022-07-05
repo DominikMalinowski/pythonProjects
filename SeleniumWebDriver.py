14d42711dcfc8b74cf4bdbda6fe850abdd1c05ec
/DominikMalinowski <malinowskidominik00@gmail.com>
2022-07-05 19:50:58 +0200

    update

--- SeleniumWebDriver.py
+++ SeleniumWebDriver.py
@@ -1,34 +1,59 @@
 
 # import of selenium and webdriver
+import unittest
+
 import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-
-# path to webdriver
-driver = webdriver.Chrome(service=Service(r'D:\ChromeDriver\chromedriver.exe'))
-
-# opening provided website and saving it as "Title"
-driver.get('https://jbzd.com.pl/')
-title = driver.title
-
-# test to check "Title" variable
-assert 'Jbzd.com.pl - najgorsze obrazki w internecie!' == title
-
-# closing currently open tab
-#driver.close()
-
-# closing the browser
-driver.quit()
-
-##
-# checking if page exist
-
 from selenium.common.exceptions import WebDriverException
-driver = webdriver.Chrome(service=Service(r'D:\ChromeDriver\chromedriver.exe'))
 
-try:
-    driver.get('https://xxxstwory.pln/')
-except WebDriverException:
-    print("Page doesn't exist")
+class MainTests(unittest.TestCase):
+    # setUp - perform before every test
+    def setUp(self):
+        pass
 
-##
+    #setUpClass - perform before everything
+    @classmethod
+    def setUpClass(self):
+        self.driver = webdriver.Chrome(service=Service(r'D:\ChromeDriver\chromedriver.exe'))
+
+    # test 1
+    def test_demo_login(self):
+        driver = self.driver
+        driver.get('https://demobank.jaktestowac.pl/logowanie_etap_1.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Logowanie' == title
+
+    # test 2
+    def test_demo_accounts(self):
+        driver = self.driver
+        driver.get('https://demobank.jaktestowac.pl/konta.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Konta' == title
+
+    # test 3
+    def test_demo_desktop(self):
+        driver = self.driver
+        driver.get('https://demobank.jaktestowac.pl/pulpit.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Pulpit' == title
+
+    # test 4
+    def test_demo_transfer(self):
+        driver = self.driver
+        driver.get('https://demobank.jaktestowac.pl/przelew_nowy_zew.html')
+        title = driver.title
+        print(title)
+        assert 'Demobank - Bankowość Internetowa - Przelew' == title
+
+    # tearDown - perform after every test
+    #def tearDown(self):
+        #self.driver.quit()
+
+    # tearDownClass - perform after everything else
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
