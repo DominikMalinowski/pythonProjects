6b849543ca9dd5c97c8abb8a65e56b7a8a7e8c74
/DominikMalinowski <malinowskidominik00@gmail.com>
2022-07-12 21:03:58 +0200

    update

--- jaktestowac.pl/demo_tests/auto_test_3.py
+++ jaktestowac.pl/demo_tests/auto_test_3.py
@@ -1,4 +1,3 @@
-
 # import of selenium and webdriver
 import unittest
 
@@ -6,6 +5,9 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 class MainTests(unittest.TestCase):
     def setUp(self):
@@ -25,7 +27,6 @@ class MainTests(unittest.TestCase):
         self.assertEqual(expected_title, title,
                          f'Expected title: ({expected_title}) differ from actual title ({title}) for page url {url}')
 
-
     def test_demo_accounts(self):
         driver = self.driver
         url = 'https://demobank.jaktestowac.pl/konta.html'
@@ -42,7 +43,7 @@ class MainTests(unittest.TestCase):
         driver.get(url)
         title = driver.title
         print(f'Actual title : {title}')
-        expected_title ='Demobank - Bankowość Internetowa - Pulpit'
+        expected_title = 'Demobank - Bankowość Internetowa - Pulpit'
         self.assertEqual(expected_title, title,
                          f'Expected title: ({expected_title}) differ from actual title ({title}) for page url {url}')
 
@@ -56,10 +57,53 @@ class MainTests(unittest.TestCase):
         self.assertEqual(expected_title, title,
                          f'Expected title: ({expected_title}) differ from actual title ({title}) for page url {url}')
 
-    #def tearDown(self):
-        #self.driver.quit()
+    # def tearDown(self):
+    # self.driver.quit()
 
     @classmethod
     def tearDownClass(self):
         self.driver.quit()
 
+
+class LoginPageTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.driver = webdriver.Chrome(service=Service(r'D:\ChromeDriver\chromedriver.exe'))
+
+    def test_page_title_text(self):
+
+        driver = self.driver
+        url = 'https://demobank.jaktestowac.pl/logowanie_etap_1.html'
+        driver.get(url)
+
+        header_element_title = driver.find_element(By.XPATH, '//*[@id="header_1"]')
+        header_page_title = header_element_title.text
+
+        expected_title = 'Wersja demonstracyjna serwisu demobank'
+
+        self.assertEqual(expected_title, header_page_title,
+                         f'Expected title: ({expected_title}) differ from \
+                         actual title ({header_page_title}) for page url {url}')
+
+    def test_if_continue_button_is_active(self):
+
+        driver = self.driver
+        url = 'https://demobank.jaktestowac.pl/logowanie_etap_1.html'
+        driver.get(url)
+
+        login_next_button_element = driver.find_element(By.XPATH, '//*[@id="login_next"]')
+
+        login_next_button_element_disabled = login_next_button_element.get_property('disabled')
+        print(f'Disabled value before: "{login_next_button_element_disabled}"')
+
+        login_input_element = driver.find_element(By.XPATH, '//*[@id="login_id"]')
+        login_input_element.send_keys('123456789', Keys.ENTER)
+
+        login_next_button_element_disabled = login_next_button_element.get_property('disabled')
+        print(f'Disabled value after: "{login_next_button_element_disabled}"')
+
+
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
