72026d679d895b524b83492cf8a14e85ea29dc35
/DominikMalinowski <malinowskidominik00@gmail.com>
2022-10-09 20:10:36 +0200

    Update

--- Wrapping.py
+++ Wrapping.py
@@ -1,20 +1,32 @@
 import datetime
+import time
 
-def datetime_wrapper(function):
-    def wrapper():
-        print(datetime.datetime.now())
-        function()
-        print(datetime.datetime.now())
+def time_wrapper(function): # function invoke for wrapping
+    def wrapper(*args): #wrapping function
+        print(f'Start: {datetime.datetime.now()}')
+        result = function(*args) #saving results to variable
+        print(f'End: {datetime.datetime.now()}')
+        return result #function return results saved as varaible
+    return wrapper #upper function return whole wrapper
 
-    return wrapper
+@time_wrapper #wrapping the functionality
+def get_sum(value_1, value_2):
+    time.sleep(2)
+    return value_1 + value_2
 
-# @datetime_wrapper
-def print_greetings():
-   workers = ['Eve', 'Bob', 'Jess', 'Pat', 'Andrea', 'Gary']
-   for worker in workers:
-       print(f'Hello {worker}!')
+@time_wrapper
+def get_multiplication(value_1, value_2):
+    time.sleep(2)
+    return value_1 * value_2
 
-# print_greetings()
 
-datetime_wrapped_greeting = datetime_wrapper(print_greetings)
-datetime_wrapped_greeting()
+value_1 = 2
+value_2 = 3
+
+# invoke wrapped function
+print(f'Sum of {value_1} and {value_2} =')
+print(get_sum(value_1, value_2))
+
+print(f'Multiplication of {value_1} and {value_2} =')
+print(get_multiplication(value_1, value_2))
+
