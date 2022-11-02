4306eeb1ca63acd98cebf5fc501dbe5dd488c18b
/DominikMalinowski <malinowskidominik00@gmail.com>
2022-11-02 21:41:27 +0100

    Update

--- jaktestowac.pl/demo_tests/lost_hat_product_details_tests.py
+++ jaktestowac.pl/demo_tests/lost_hat_product_details_tests.py
@@ -1,4 +1,4 @@
-import time
+from selenium.webdriver.support.select import Select
 
 from helpers.base_test_class import BaseTestClass
 from helpers.wrappers import screenshot_decorator
@@ -39,13 +39,27 @@ class LostHatProductDetailsTests(BaseTestClass):
         option_l_element = driver.find_element(By.XPATH, size_l_select_xpath)
         option_l_element.click()
 
-        observed_product_size_changed_value = product_size_select_element.get_property('value')
+        observed_product_size_changed_value = product_size_select_element.get_attribute('value')
         observed_product_size_changed_value_element = driver.find_element(By.XPATH,
                                                                           f'//*[@id="group_1"]/*[@value="{observed_product_size_changed_value}"]')
-        observed_product_size_changed_text = observed_product_size_changed_value_element.text
+        observed_product_size_changed_value_element_text = observed_product_size_changed_value_element.text
 
         self.assertNotEqual(expected_product_default_size, observed_product_default_size_text,
                             f'Expected size is the same as actual on page: {driver.current_url}')
 
-        self.assertEqual(expected_product_default_size, observed_product_size_changed_text,
+        self.assertEqual(expected_product_default_size, observed_product_size_changed_value_element_text,
                          f'Expected size differ from actual on page: {driver.current_url}')
+
+    @screenshot_decorator
+    def test_check_product_size_change_new(self):
+        expected_product_changed_size = 'L'
+        size_select_xpath = '//*[@id="group_1"]'
+        url = 'https://autodemo.testoneo.com/en/men/1-4-hummingbird-printed-t-shirt.html'
+
+        driver = self.ef_driver
+        driver.get(url)
+
+        product_size_select_element = driver.find_element(By.XPATH, size_select_xpath)
+        product_size_select = Select(product_size_select_element)
+        observed_product_default_size_text = product_size_select.first_selected_option.text
+        print(observed_product_default_size_text)
