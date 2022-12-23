a9cb4c724bcc4ed7797b999e37cdbb93b2848a1d
DominikMalinowski <malinowskidominik00@gmail.com>
2022-12-23 18:25:04 +0100

    update

--- Data Managment/Data_Managment_test.py
+++ Data Managment/Data_Managment_test.py
@@ -2,11 +2,16 @@ import unittest
 import DataManagment_2 as DM2
 import os 
 
-class Data_Managment_Test():
-    def is_abs_path_test():
-        if os.path.isabs(DM2.new_folder_abspath) == True:
-            print('ok')
-        else: 
-            print('test has failed!')
+class Data_Managment_Test(unittest.TestCase):
+	def is_abs_path_test(new_folder_abspath):
+		if os.path.isabs(DM2.new_folder_abspath) == True:
+			print('ok')
+		else: 
+			print('test has failed!')
 
-
+        
+	def is_path_exist_test():
+		if os.path.exists(DM2.new_folder_path) == True:
+			pass
+		else:
+			print("Test failed")
