98bda9936d067f4ea693de0397b7ab73c28bca27
DominikMalinowski <malinowskidominik00@gmail.com>
2023-01-12 17:58:00 +0100

    update

---
+++ Andea/Exercise_5/Exercise_5.py
@@ -0,0 +1,22 @@
+"""
+Write a Python script to concatenate following dictionaries to create a new one.
+
+Sample Dictionary :
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+Expected Result : {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+"""
+
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+result_dictionary = {}
+
+    
+def concatenate_dictionary(*dictionary):
+    for dic in dictionary:
+        result_dictionary.update(dic)
+    return result_dictionary
+
+print(concatenate_dictionary(dic1,dic2,dic3))
