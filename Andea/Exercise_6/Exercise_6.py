a589d76580b6218dbfff7065675a0a01724b01e5
DominikMalinowski <malinowskidominik00@gmail.com>
2023-03-20 20:57:46 +0100

    update

--- Andea/Exercise_6/Exercise_6.py
+++ Andea/Exercise_6/Exercise_6.py
@@ -2,27 +2,27 @@
 adjust your solution for chapter 4, and allow user to put the list with ; as a delimiter.
 You should also remove white spaces from the imput.
 """
-
-
-sample_List = [' Red ; Green ; White ; Black ; Pink ; Yellow ']
+# Input list with ; as delimiter
+sample_List_2 = [' Red; Green; White; Black; Pink; Yellow ']
 elements_to_remove = [1,4,5]
 erase_list = []
 
-input_list = input()
-new_list = [remodeled_item_list.strip() for item in input_list]
+# Change list into string, strip and replace ; 
+sample_List_string = sample_List_2[0]
+list_stripped_and_replaced = sample_List_string.strip().replace(';',',')
+sample_List = list_stripped_and_replaced.split(',')
 
+# creating list of elements that have to be removed 
+for i in range(len(sample_List)):
+    if i in elements_to_remove:
+        erase_list.append(sample_List[i])
 
-# # creating list of elements that have to be removed 
-# for i in range(len(sample_List)):
-#     if i in elements_to_remove:
-#         erase_list.append(sample_List[i])
+# removing selected elements from the list 
+for element in erase_list:
+    if element in sample_List:
+        sample_List.remove(element)
 
-# # removing selected elements from the list 
-# for element in erase_list:
-#     if element in sample_List:
-#         sample_List.remove(element)
-
-# # sorting list 
-# sample_List.sort()
-# print(sample_List)
+# sorting list 
+sample_List.sort()
+print(sample_List)
 
