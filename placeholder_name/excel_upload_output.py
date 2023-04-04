8637edc96691aa91e83b1396150288fb0ebef39f
DominikMalinowski <malinowskidominik00@gmail.com>
2023-04-04 22:12:02 +0200

    update

--- placeholder_name/excel_upload_output.py
+++ placeholder_name/excel_upload_output.py
@@ -1,14 +1,13 @@
 import openpyxl
+from openpyxl import utils 
 
 wb = openpyxl.load_workbook('example.xlsx')
-print(type(wb))
 
 # get all sheets name 
 print(wb.get_sheet_names())
 
 #saving specyfic sheet as variable
 sheet_3 = wb.get_sheet_by_name('Sheet3')
-print(sheet_3.title)
 
 # getting active sheet from the file 
 active_sheet = wb.active
@@ -24,3 +23,27 @@ print(A1.column)
 
 for i in range (1, 8, 2):
     print(i, active_sheet.cell(row=i, column=2).value)
+
+# getting data from specyfic row/column 
+placeholder = active_sheet.cell(row = 1, column = 2).value
+
+# getting max row/column number 
+row_count = active_sheet.max_row
+column_count = active_sheet.max_column
+
+print(row_count, column_count)
+
+# getting #n column name(letter) 
+print(utils.get_column_letter(115))
+
+# getting column # from name
+print(utils.column_index_from_string('DK'))
+
+# displaying all data in range 
+for rowOfCellObjects in active_sheet['A1':'C3']:
+    for cellObj in rowOfCellObjects:
+        print(cellObj.coordinate, cellObj.value)
+    print('Koniec')
+
+
+
