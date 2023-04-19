c64f13a81bbb448b08144c2d1757498bd944783f
DominikMalinowski <malinowskidominik00@gmail.com>
2023-04-19 21:28:34 +0200

    update

---
+++ Cipher/placeholder.py
@@ -0,0 +1,17 @@
+
+def input_method_selection():
+    options = ['Caesar shift','The Great Paris Cipher', 'Morse Code']
+    input_message = "Please select you cipher method:\n"
+    user_input = ''
+
+    for index, item in enumerate(options):
+        input_message += f'{index+1}) {item}\n'
+
+    while user_input not in map(str, range(1, len(options) + 1)):
+        user_input = input(input_message)
+
+    return user_input
+
+
+
+input_method_selection()
