1b64eac1b0f684e7545afeca0441c01ac6748710
DominikMalinowski <malinowskidominik00@gmail.com>
2023-08-16 18:14:59 +0200

    update

--- Cipher/modules/vigenere.py
+++ Cipher/modules/vigenere.py
@@ -1,13 +1,1 @@
-
-def vigenere(text, key):
-    vigenere_text = []
-
-    for i in range(len(text)):
-        x = (ord(text[i]) +
-             ord(key[i])) % 26
-        x += ord('A')
-        vigenere_text.append(chr(x))
-    return("".join(vigenere_text))
-     
-vigenere('dupa','zady')
-
+print('text')
