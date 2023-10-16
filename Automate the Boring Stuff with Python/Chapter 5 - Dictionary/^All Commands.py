85d8c9b7284c00a5c9c862d1fe973a56e02cb1d4
DOMINIK MALINOWSKI <malinowskidominik00@gmail.com>
2023-10-16 09:57:51 +0200

    update

--- Automate the Boring Stuff with Python/Chapter 5 - Dictionary/^All Commands.py
+++ Automate the Boring Stuff with Python/Chapter 5 - Dictionary/^All Commands.py
@@ -1,1 +1,15 @@
 # Content:
+# -  keys(), values() and items() for dictionary 
+# - get() for dictionary 
+
+
+
+# keys(), values() and items() for dictionary 
+dic = {'key1':'value1','key2':'value2','key3':'value3'}
+print(dic.keys())
+print(dic.values())
+print(dic.items())
+
+# get() for dictionary 
+dic = {'key1':'value1','key2':'value2','key3':'value3'}
+print(dic.get('key2'))
