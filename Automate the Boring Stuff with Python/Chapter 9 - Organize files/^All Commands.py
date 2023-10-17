c2e346ef03d56b094af9ba9711ef0ea17ec22a3a
DOMINIK MALINOWSKI <malinowskidominik00@gmail.com>
2023-10-17 15:33:11 +0200

    update

--- Automate the Boring Stuff with Python/Chapter 9 - Organize files/^All Commands.py
+++ Automate the Boring Stuff with Python/Chapter 9 - Organize files/^All Commands.py
@@ -1,7 +1,35 @@
 # Content:
-# - 
+# - change directory 
+# - copy file 
+# - copy file with all content
+# - move file 
+# - remove file/directory
+# - save remove file/directory
 
+# change directory 
+import os 
+os.chdir('{new path}')
 
+# copy file 
+import os 
+import shutil
+shutil.copy('{source}','{destination}')
 
+# copy file with all content
+import shutil
+shutil.copytree('{source}','{destination}')
+
+# move file 
+import shutil
+shutil.move('{source}','{destination}')
+
+# remove file/directory
+import os
+os.unlink('{path}') # remove all data
+os.rmdir('{path}') # remove only empty folders
+
+# save remove file/directory
+import send2trash
+send2trash.send2trash('{path}')
 
 # 
