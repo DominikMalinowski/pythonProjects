f3ea71b73930000dec717a9070e4a8f220805450
DOMINIK MALINOWSKI <malinowskidominik00@gmail.com>
2023-11-20 15:41:17 +0100

    update

--- Automate the Boring Stuff with Python/Chapter 13 - PDF + docx/^All Commands - docx.py
+++ Automate the Boring Stuff with Python/Chapter 13 - PDF + docx/^All Commands - docx.py
@@ -1,2 +1,16 @@
 
-#
+# creating document object 
+import docx
+doc = docx.Document('demo.docx')
+
+# getting text from paragraph
+import docx
+doc = docx.Document('demo.docx')
+len(doc.paragraphs)
+doc.paragraph[0].text
+
+# getting text from runs
+import docx
+doc = docx.Document('demo.docx')
+len(doc.paragraphs[1].runs)
+doc.paragraph[1].runs[3].text
