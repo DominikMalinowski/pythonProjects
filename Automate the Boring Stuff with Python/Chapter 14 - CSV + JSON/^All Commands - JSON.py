cd000771fbeb6032a1403bc0c08fb167d2f9170c
DOMINIK MALINOWSKI <malinowskidominik00@gmail.com>
2023-11-23 12:31:31 +0100

    update

--- Automate the Boring Stuff with Python/Chapter 14 - CSV + JSON/^All Commands - JSON.py
+++ Automate the Boring Stuff with Python/Chapter 14 - CSV + JSON/^All Commands - JSON.py
@@ -1,1 +1,13 @@
 
+# load JSON data 
+jsonString = '{"name": "Sophie", "isCat": true, "miceCaught":0, "felineIQ":null}'
+
+import json
+jsonData = json.loads(jsonString)
+print(jsonData)
+
+# save data as JSON 
+pythonString = {'name': 'Sophie', 'isCat': True, 'miceCaught':0, 'felineIQ': None}
+import json
+jsonDataFromString = json.dumps(pythonString)
+print(jsonDataFromString)
