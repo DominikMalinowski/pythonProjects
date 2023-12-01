e5dcddaf2d864bbbed2ec721abddd14f3800ecb0
DOMINIK MALINOWSKI <malinowskidominik00@gmail.com>
2023-12-01 14:44:34 +0100

    update

--- Selenium + Python - Novice to Ninja/test.py
+++ Selenium + Python - Novice to Ninja/test.py
@@ -1,9 +1,43 @@
 
+city = 'nyc'
+event = 'show'
+
+print("Welcome to %s and enjoy the %s" %(city,event))
+
+
+l1 = [1,2,3]
+l2 = [4,5,6,7,8]
+
+for a,b in zip(l1,l2):
+    print(a,b)
+
+# %%
+def add(a1,a2):
+    return(a1+a2)
+
+print(add(2,4))
+
 # %%
 
-import os 
-path =['one','two']
-for i in path:
-    print(i)
+a = 10 
 
-# %% 
+def change():
+    global a 
+    print('2. Value of "a" is:' + str(a))
+    a = 2 
+    print('3. Value of "a" is:' + str(a))
+
+print('1. Value of "a" is:' + str(a))
+change()
+print('4. Value of "a" is:' + str(a))
+# %%
+
+def largest(*args):
+    print(max(args))
+    
+a = [1,2,3,4]
+b = [2,3,6]
+c = [20,30,50]
+
+largest(a,b)
+# %%
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 
-# opening encrypted file (decrypt)
-import PyPDF2
-pdfReader = PyPDF2.PdfReader(open('placeholder.pdf','rb'))
-pdfReader.is_encrypted #'True' if file is encrypted 
-pdfReader.decrypt('rosebud') #decrypt with known password 
-pageObj = pdfReader.pages[0]
-pageObj.extract_text()
+
+# %%
+
+import os 
+path =['one','two']
+for i in path:
+    print(i)
+
+# %% 
+print('boo')
