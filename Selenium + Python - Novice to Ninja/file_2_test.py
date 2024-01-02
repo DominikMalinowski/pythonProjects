0c080270a6a8cd48622c5c6566b17580fb680c35
DOMINIK MALINOWSKI <malinowskidominik00@gmail.com>
2024-01-02 14:34:39 +0100

    update

--- Selenium + Python - Novice to Ninja/file_2_test.py
+++ Selenium + Python - Novice to Ninja/file_2_test.py
@@ -1,23 +1,23 @@
 import unittest
 
-class TestCase(unittest.TestCase):
+class TestCase2(unittest.TestCase):
 
     def setUp(self):
-        print('SetUp before every test')
+        print('SetUp before every test for second file')
 
     def tearDown(self):
-        print('TearDown after every test')
+        print('TearDown after every test for second file')
 
-    def test_method_A(self):
+    def test_method_B(self):
         print('Test method A')
     
-    def test_assert(self):
+    def test_assert2(self):
         a = True
         self.assertTrue(a, 'a is not true')
 
-    def test_equal(self):
-        a = 'test'
-        b = 'test'
+    def test_equal2(self):
+        a = 'second'
+        b = 'second'
         self.assertEqual(a,b, msg='Values are not equal')
     
 if __name__ == '__main__':
